3e926bdfcd958389db02f6b45f77e1d1febfde8a
Patrik Banko <patrikbanko@gmail.com>
2022-02-23 21:25:53 +0100

    Akcelerometar

--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,9 +1,18 @@
-import { fromObject } from '@nativescript/core'
+import { fromObject, Observable } from '@nativescript/core'
+import { startAccelerometerUpdates } from "nativescript-accelerometer"
 
 export function HomeViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
+    const viewModel = new Observable()
+    viewModel.x = 10
+    viewModel.y = 20
+    viewModel.z = 30
+
+    var accelerometer = require("nativescript-accelerometer")
+    accelerometer.startAccelerometerUpdates(function(data) {
+        viewModel.set('x','x = ' + data.x.toFixed(3))
+        viewModel.set('y', 'y = ' + data.y.toFixed(3))
+        viewModel.set('z', 'z = ' +  data.z.toFixed(3))
+  }, { sensorDelay: "ui" })
 
   return viewModel
 }
